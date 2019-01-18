8f19c663798abfa8be702f1a9afd2d8e2e154eb3
Boyeon.Choi <boyeon.choi@icon.foundation>
2019-01-18 13:53:42 +0900

    Update travis

--- setup.py
+++ setup.py
@@ -53,6 +53,5 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6'
-        'Programming Language :: Python :: 3.7'
     ]
 )
